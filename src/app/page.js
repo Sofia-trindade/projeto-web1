4193b80fc4a9e3f485dc0eb28f5a6624d8ab770f
jenifferesther <126255280+vitor-gss@users.noreply.github.com>
2023-11-22 14:46:30 -0300

    adiciona titulo

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,18 @@
 // import Image from 'next/image'
 import styles from './page.module.css'
 
+function Titulo ({x, y}){
+  return(
+    <h2 style={{left:x, top:y}} className={styles.titulo}>
+      Best house plants varieties.
+    </h2>
+  )
+}
+
 export default function Home() {
   return (
-    <main className={styles.main}>
-      
+    <main>
+      <Titulo x={26.26} y={119.24}/>
     </main>
   )
 }
