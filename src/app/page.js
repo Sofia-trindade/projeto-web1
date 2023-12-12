595fb828ef8d4b7ebea868a9f68c117b2b5c7c0b
Sofia Trindade <sofiaatrindadee02@gmail.com>
2023-12-12 11:32:16 -0300

    Tentativa

--- src/app/page.js
+++ src/app/page.js
@@ -10,27 +10,41 @@ import planta1 from '../../public/planta1.png'
 
 function Planta1 ({x, y}) {
   return (
-    <div style={{left:x, top:y}} className={styles.planta1}>
-      <Image src={planta1}/>
-    </div>
+  
+      <div style={{left:x, top:y}} className={styles.planta1}>
+        <Image src={planta1}/>
+      </div>
+   
   );
+ 
+  
+    
 }
 
-// function Titulo ({x, y}){
-//   return(
-//     <h2 style={{left:x, top:y}} className={styles.titulo}>
-//       Best house plants varieties.
-//     </h2>
-//   );
-// } 
+function Titulo ({x, y}){
+  return(
+    <h2 style={{left:x, top:y}} className={styles.titulo}>
+      Best house plants varieties.
+    </h2>
+  );
+} 
 
-// function Botao ({x, y}) {
-//   return (
-//      <div style={{left:x, top:y}} className={styles.botao}>
-//       <Image src={botão}/>
-//      </div>
-//    );
-//  }
+function Frame2 ({children, x, y}) {
+  return(
+    <div style={{left:x, top:y}} className={styles.div}>
+      {children}
+    </div>
+  )
+}
+
+
+function Botao ({children, x, y}) {
+  return (
+     
+      <button style={{left:x, top:y}} className={styles.botao}>{children}</button>
+     
+   );
+ }
 //  function Logos({x, y}){
 //   return(
 //     <div style={{left: x , top: y}} className={styles.logos}>
@@ -61,8 +75,22 @@ export default function Home() {
   return (
     <main>
       <Planta1 x={0} y={58.2}/> 
-      {/* <Titulo x={26.26} y={119.24}/>
-       <Botao x={29} y={312.29}/>
+      <Titulo x={26.26} y={119.24}/>
+      <Frame2 x={0} y={329}>
+      <div>
+        <p style={{left:31.91, top:37.02}} className={styles.p}>Beautiful living greenery for homes and offices</p>
+      </div>
+
+      <div>
+      <p style={{left:31.91, top:156.36}} className={styles.p1}>Bloomberg</p>
+      </div>
+
+      <div>
+      <p style={{left:210.6, top:156.36}} className={styles.p2}>Forbes</p>
+      </div>
+      <Botao x={343.35} y={42.76}>Shop now</Botao>
+      </Frame2>
+      {/*<Botao x={29} y={312.29}/>
       <SubTitulo x={31.91} y={37.02}/>
       <Frase x={35.49} y={479.2}/> 
       <Logo x={31.91} y={156.36}/> */}
